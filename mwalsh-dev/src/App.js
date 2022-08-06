f0241681ee2bc8ea9e2c23a46ab711772ae9fcd9
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-08-06 18:42:07 +0000

    Fixed build deployment issues

--- mwalsh-dev/src/App.js
+++ mwalsh-dev/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
     return (
-        <h1>Hello World</h1>
+        <Router>
+            <Routes>
+                <Route path='/' exact element={
+                    <h1>Hello World</h1>
+                } />
+            </Routes>
+        </Router>
     );
 }
 
